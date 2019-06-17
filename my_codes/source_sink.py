f480419c4eb92265a39e602b4ae12e1adb405e1f
bharath-kamath705 <49598128+bharath-kamath705@users.noreply.github.com>
2019-06-17 10:55:26 -0500

    Update source_sink.py

--- my_codes/source_sink.py
+++ my_codes/source_sink.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Source-Sink
 """
@@ -8,20 +7,34 @@ import numpy
 from matplotlib import pyplot
 
 
-N=50                          #total grid points in each direction
+N=20                          #total grid points in each direction
 x_start, x_end = -2.0, 2.0    # x direction boundaries
 y_start, y_end = -2.0, 2.0    # y direction boundaries
 x = numpy.linspace(x_start,x_end,N)  # 1D array of x co-ordinates
 y = numpy.linspace(y_start,y_end,N)  # 1D array of y co-ordinates
 
+X, Y = numpy.meshgrid(x, y)   # generate grid
 
-X, Y = numpy.meshgrid(x, y)   #generate grid
 
-# plot grid
-width = 10.0                  #graph width on screen in cm
-height = (y_end - y_start) / (x_end - x_start) * width #graph height
-pyplot.figure(figsize=(width, height)) #create figure
-pyplot.xlim(x_start, x_end) 
+x_s, y_s = -1.0, 0.0          # source location
+S_str = 5.0                   # source strength
+
+# radial distance and angle from source
+r   = numpy.sqrt( (X-x_s)**2 + (Y-y_s)**2)
+phi = numpy.arctan2(Y-y_s, X-x_s)
+
+# velocity field
+u = numpy.cos(phi)*S_str/r
+v = numpy.sin(phi)*S_str/r
+
+# plot the streamlines
+width = 10.0
+height = (y_end - y_start) / (x_end - x_start) * width
+pyplot.figure(figsize=(width, height))
+pyplot.xlabel('x', fontsize=16)
+pyplot.ylabel('y', fontsize=16)
+pyplot.xlim(x_start, x_end)
 pyplot.ylim(y_start, y_end)
-pyplot.scatter(X, Y, s=5, color='blue', marker='o')
-
+pyplot.quiver(X, Y, u, v)
+pyplot.scatter(x_s, y_s,
+               color='#CD2305', s=80, marker='o');
